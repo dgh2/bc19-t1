a9be9fa4bf971f267498ad977b0c8c82b6630cfd
GHolt <2525915+dgh2@users.noreply.github.com>
2019-01-11 17:58:00 -0600

    Move credentials for uploading into bc19.properties
    bc19.properties should have the format:
    BC_USERNAME=*
    BC_PASSWORD=*

--- upload.bat
+++ upload.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 echo Uploading, please wait:
 setlocal
-set BC_USERNAME=*
-set BC_PASSWORD=*
+FOR /F "tokens=1,2 delims==" %%G IN (bc19.properties) DO (set %%G=%%H)
+set BC_USERNAME=%BC_USERNAME%
+set BC_PASSWORD=%BC_PASSWORD%
 @ECHO ON
 call bc19upload -i compiled_bot.js
 @ECHO OFF
