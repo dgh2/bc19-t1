2cf10a4239f134cac072ace57d9400961e2fcb67
GHolt <2525915+dgh2@users.noreply.github.com>
2019-01-11 18:18:48 -0600

    Upload renamed bot instead of first compiled
    compiling generates new .js instead of overwriting, so upload upload_bot.js instead of the first compiled bot

--- upload.bat
+++ upload.bat
@@ -5,7 +5,7 @@ FOR /F "tokens=1,2 delims==" %%G IN (bc19.properties) DO (set %%G=%%H)
 set BC_USERNAME=%BC_USERNAME%
 set BC_PASSWORD=%BC_PASSWORD%
 @ECHO ON
-call bc19upload -i compiled_bot.js
+call bc19upload -i upload_bot.js
 @ECHO OFF
 endlocal
 echo.
