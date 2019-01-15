4171679492e92ffa6ccfaf7d513ed09bc8f9290d
GHolt <2525915+dgh2@users.noreply.github.com>
2019-01-15 12:55:00 -0600

    get enemy info for crusaders

--- bots/jstest/crusader.js
+++ bots/jstest/crusader.js
@@ -7,6 +7,13 @@ var dir = null;
 crusader.turn = (self) => {
     //self.log("Health: " + self.me.health);
     let step = self.step;
+
+    let closestEnemies = nav.getVisibleRobots(self, enemyTeam);
+    let closestEnemyAttacker = nav.getVisibleRobots(self, enemyTeam, [SPECS.CRUSADER, SPECS.PROPHET, SPECS.PREACHER]);
+    let closestEnemyPilgrims = nav.getVisibleRobots(self, enemyTeam, SPECS.PILGRIM);
+    let closestEnemyBuilding = nav.getVisibleRobots(self, enemyTeam, [SPECS.CASTLE, SPECS.CHURCH]);
+    let closestEnemyCastle = nav.getVisibleRobots(self, enemyTeam, SPECS.CASTLE);
+
     if (dir === null || !nav.isPassable(self, {x: self.me.x + dir.x, y: self.me.y + dir.y})) {
         dir = nav.randomValidDir(self);
     }
